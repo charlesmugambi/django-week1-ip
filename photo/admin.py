6a1ed73e17484d8449b1ee910cfc10bdd29c82a7
charlesmicheni <charlesmugambi100@gmail.com>
2020-11-16 08:57:25 +0300

    the admin

---
+++ photo/admin.py
@@ -0,0 +1,11 @@
+
+
+# Register your models here.
+from django.contrib import admin
+from .models import Picture, Category, Location
+
+
+
+admin.site.register(Picture)
+admin.site.register(Category)
+admin.site.register(Location)
